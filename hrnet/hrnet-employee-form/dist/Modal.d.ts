b3cdb5887c3b85ba72897c16d4dd9bfe96290e48
jeanmax1me <jeanmaximegilbert@gmail.com>
2024-10-09 11:14:59 +0200

    niinoionGESG

--- hrnet/hrnet-employee-form/dist/Modal.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-type ModalProps = {
-    isOpen: boolean;
-    onClose: () => void;
-    children: React.ReactNode;
-};
-declare const Modal: React.FC<ModalProps>;
-export default Modal;
